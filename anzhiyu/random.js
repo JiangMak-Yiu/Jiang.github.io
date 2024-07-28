c31cc645cc30b07b9b0d1dc2e5f3219b521759d8
Jiang <jiangjiangbs@qq.com>
2024-07-28 12:12:50 +0800

    Site updated: 2024-07-28 12:12:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/27/4DDiG/","2024/07/27/hello-world/","2024/07/27/完美解码/"];function toRandomPost(){
+var posts=["2024/07/27/4DDiG/","2024/07/28/DocKer/","2024/07/28/图床/","2024/07/27/clash/","2024/07/27/完美解码/","2024/07/28/攻击/","2024/07/28/清理alist/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
